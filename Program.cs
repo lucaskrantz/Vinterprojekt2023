40524de713eab9f05e93316261cf8351e9fcb8d5
Lucas Krantz <lucas.krantz@elev.ga.ntig.se>
2023-11-24 11:14:17 +0100

    Startat bygga timer

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ Raylib.SetTargetFPS(60);
 Cube cube = new Cube();
 Game game = new Game();
 List<Platform> platforms = new List<Platform>();
-
+float Timer = 3f;
+float TimerMax = 3f;
+void ResetTimer() // Startar om rock-timern
+{
+    Timer = TimerMax;
+}
 void AddPlatform(List<Platform> platforms)
 {
     platforms.Add(new Platform(600, 600, 200, 20));
@@ -18,6 +23,8 @@ while (Raylib.WindowShouldClose() == false)
     cube.SetonGroundToFalse();
     game.CheckCollision(cube.rect, ref cube.onGround, ref cube.velocity, platforms);
     cube.Update();
+    Timer -= Raylib.GetFrameTime();
+
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.WHITE);
